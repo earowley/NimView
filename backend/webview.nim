70439e67e111b88856a7a21efed23008d1553390
Eric Rowley <earowley23@gmail.com>
2024-02-18 15:37:39 -0600

    Basic binding template for Nim functions

--- backend/webview.nim
+++ backend/webview.nim
@@ -1,14 +1,15 @@
 import std/os
+import std/macros
+import std/json
 import futhark
 import ./buildmeta
 
 const
   webviewBase = bdir / "lib" / "webview"
-  linkerFlags = case hostOS
-  of "macosx":
+  linkerFlags = when hostOS == "macosx":
     "-framework WebKit"
   else:
-    ""
+    {.error: "Unsupported OS: " & hostOS.}
 
 importc:
   path webviewBase
@@ -50,3 +51,46 @@ proc navigate*(self: Window, url: string) =
 
 proc run*(self: Window) =
   webview_run(self.raw)
+
+template argsTupleImpl(val: NimNode): NimNode =
+  val.expectKind(nnkSym)
+  let tt = val.getTypeImpl()
+  tt.expectKind(nnkProcTy)
+  var params = newSeq[NimNode]()
+  for param in tt[0]:
+    if param.kind == nnkIdentDefs:
+      params.add(param[1])
+  let result = nnkTupleConstr.newNimNode()
+  for param in params:
+    result.add(param)
+  result
+
+macro callFromParsed(fun, parsed: typed): untyped =
+  let argTypes = parseExpr(argsTupleImpl(fun).repr)
+  var callParams = newSeq[NimNode]()
+  let to = bindSym("to")
+  let jnidx = bindSym("[]")
+  for i in 0..<argTypes.len:
+    let indexer = newCall(
+      jnidx,
+      parsed,
+      newLit(i)
+    )
+    let toExpr = newCall(
+      to,
+      indexer,
+      argTypes[i]
+    )
+    callParams.add(toExpr)
+  result = newCall(fun, callParams)
+
+template `bind`*(self: Window, jsName: string, callback: typed): untyped =
+  proc glue(id, jsonArgs: cstring, cxt: webview_t) {.cdecl, global.} =
+    let parsed = parseJson($jsonArgs)
+    type ResultType = typeof(callFromParsed(callback, parsed))
+    when ResultType is void:
+      callFromParsed(callback, parsed)
+    else:
+      let callbackResult = $(%*(callFromParsed(callback, parsed)))
+      webview_return(cxt, id, 0, callbackResult.cstring)
+  webview_bind(self.raw, jsName.cstring, glue, self.raw)
